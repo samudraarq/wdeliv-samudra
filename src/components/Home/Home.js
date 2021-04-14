a2fb9ce90fafd9f41342aec8e207ea9e5d76ecdd
Samudra Faria <samudrafaris@gmail.com>
2021-04-14 17:25:27 +0700

    implement infinite scroll

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,29 +1,38 @@
 import Card from "./Card/Card";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import InfiniteScroll from "react-infinite-scroller";
+
 import styles from "./Home.module.css";
 import Container from "../UI/Container";
 
 const Home = () => {
-  const currentPage = 1;
   const [products, setProducts] = useState([]);
-  useEffect(() => {
+
+  const fetchData = (page) => {
     axios
       .get(
-        `https://zax5j10412.execute-api.ap-southeast-1.amazonaws.com/dev/api/product/list?page=${currentPage}`
+        `https://zax5j10412.execute-api.ap-southeast-1.amazonaws.com/dev/api/product/list?page=${page}`
       )
       .then(function (response) {
-        setProducts(response.data.value.products);
+        setProducts([...products, ...response.data.value.products]);
       });
-  }, []);
+  };
 
   return (
     <Container>
-      <div className={styles.container}>
-        {products.map((product) => (
-          <Card product={product} key={product.id} />
-        ))}
-      </div>
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={fetchData}
+        hasMore={true}
+        loader={<div className="loader">Loading ...</div>}
+      >
+        <div className={styles.container}>
+          {products.map((product) => (
+            <Card product={product} key={product.id} />
+          ))}
+        </div>
+      </InfiniteScroll>
     </Container>
   );
 };
